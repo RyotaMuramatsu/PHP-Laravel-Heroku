9f310f4453a24db72fe68095702fc3c4494551e0
Ryota <0409muramatsu@gmail.comgit config -luser.name=Ryotauser.email=0409muramatsu@gmail.com>
2020-08-19 00:26:16 +0000

    ProfileController

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -1,4 +1,5 @@
 <?php
+// カリキュラム課題の再提出です
 
 namespace App\Http\Controllers\Admin;
 
@@ -7,5 +8,24 @@ use App\Http\Controllers\Controller;
 
 class ProfileController extends Controller
 {
-    //
+    //以下に課題を追記しています
+    public function add()
+    {
+        return view('admin.profile.create');
+    }
+    
+    public function create()
+    {
+        return redirect('admin/profile/create');
+    }
+    
+    public function edit()
+    {
+        return view('admin.profile.edit');
+    }
+    
+    public function update()
+    {
+        return redirect('admin/profile/edit');
+    }
 }

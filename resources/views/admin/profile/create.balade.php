88b9e53e7dfa3caa1261a27162cc53f116dcc427
Ryota <0409muramatsu@gmail.comgit config -luser.name=Ryotauser.email=0409muramatsu@gmail.com>
2020-08-22 03:37:47 +0000

    PHP/Laravel 11_kadai

--- resources/views/admin/profile/create.balade.php
+++ resources/views/admin/profile/create.balade.php
@@ -1,15 +1,14 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset='utf-8'>
-        <meta http-equiv="X-UA-compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width,inital-scale=1">
-        
-        <title>MyNews</title>
-    </head>
-    
-    <body>
-        <h1>プロフィール新規作成画面</h1>
-    </body>
-    
-</html>
+@extends('layouts.profile')
+
+@section('title', 'Myプロフィール画面')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 mx-auto">
+                <h2>My プロフィール</h2>
+            </div>
+        </div>
+    </div>
+@endsection
+

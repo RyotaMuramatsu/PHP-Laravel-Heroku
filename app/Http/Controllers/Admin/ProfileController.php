c70b19fe4d887066459b66de38e4994d6d2d6739
Ryota <0409muramatsu@gmail.comgit config -luser.name=Ryotauser.email=0409muramatsu@gmail.com>
2020-08-24 04:51:27 +0000

    php-laravel10_kadai

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -14,18 +14,18 @@ class ProfileController extends Controller
         return view('admin.profile.create');
     }
     
-    public function create()
-    {
-        return redirect('admin/profile/create');
-    }
-    
     public function edit()
     {
         return view('admin.profile.edit');
     }
     
-    public function update()
-    {
-        return redirect('admin/profile/edit');
-    }
+    // public function create()
+    // {
+    //     return redirect('admin/profile/create');
+    // }
+    
+    // public function update()
+    // {
+        // return redirect('admin/profile/edit');
+    // }
 }
